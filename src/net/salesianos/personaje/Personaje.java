2581e33f4ee0e08526e23d34ba85fda1542b1564
Kilian David <usr3744@salesianos-lacuesta.net>
2026-03-24 12:54:08 +0000

    Atributos y constructor

--- src/net/salesianos/personaje/Personaje.java
+++ src/net/salesianos/personaje/Personaje.java
@@ -1,5 +1,15 @@
 package net.salesianos.personaje;
 
 public class Personaje {
+    private String nombre;
+    private int nivel;
+    private int energia;
+
+
+public Personaje(String nombre, int nivel) {
+    this.nombre = nombre;
+    this.nivel = nivel;
+    this.energia = 100;
+}
     
 }
