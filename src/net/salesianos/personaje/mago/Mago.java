b11849605c89fc2663c323424287c4467d10fa52
Kilian David <usr3744@salesianos-lacuesta.net>
2026-03-27 08:32:41 +0000

    Atributo, constructor propio, metodo y overrdrive

--- src/net/salesianos/personaje/mago/Mago.java
+++ src/net/salesianos/personaje/mago/Mago.java
@@ -1,5 +1,25 @@
 package net.salesianos.personaje.mago;
 
-public class Mago {
+import net.salesianos.personaje.Personaje;
+
+public class Mago extends Personaje {
+
     
+    private String elemento;
+
+    
+    public Mago(String nombre, int nivel, String elemento) {
+        super(nombre, nivel);
+        this.elemento = elemento;
+    }
+
+    public void ataqueElemental() {
+        gastarEnergia(25);
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("Nivel: " + getNivel());
+        System.out.println("Elemento: " + elemento);
+    }
 }
