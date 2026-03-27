c20bf7c496c6fad7f35dfeb6f168145786be2acf
Kilian David <usr3744@salesianos-lacuesta.net>
2026-03-27 08:29:02 +0000

    Atributo, Constructor y overrdrive

--- src/net/salesianos/personaje/guerrero/Guerrero.java
+++ src/net/salesianos/personaje/guerrero/Guerrero.java
@@ -1,5 +1,27 @@
 package net.salesianos.personaje.guerrero;
 
-public class Guerrero {
+import net.salesianos.personaje.Personaje;
+
+public class Guerrero extends Personaje {
+
     
+    private String arma;
+
+    
+    public Guerrero(String nombre, int nivel, String arma) {
+        super(nombre, nivel);
+        this.arma = arma;
+    }
+
+    
+    public void ataquePesado() {
+        gastarEnergia(15);
+    }
+
+   
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Arma: " + arma);
+    }
 }
