0c11007c31b694e564a9b0c417d253f808f8096d
Kilian David <usr3744@salesianos-lacuesta.net>
2026-03-27 08:58:02 +0000

    Creamos Viajero

--- src/App.java
+++ src/App.java
@@ -1,3 +1,21 @@
+import net.salesianos.personaje.Personaje;
+import net.salesianos.personaje.guerrero.Guerrero;
+import net.salesianos.personaje.mago.Mago;
+
+
 public class App {
-    
-}
+    public static void main(String[] args) {
+
+        Personaje viajero = new Personaje("Eldrin", 3);
+
+        viajero.entrenar(4);
+
+        viajero.gastarEnergia(30);
+
+        
+        viajero.mostrarInfo();
+
+        
+  }
+
+ }
