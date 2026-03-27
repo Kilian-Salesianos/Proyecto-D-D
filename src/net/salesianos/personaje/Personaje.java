764dd2020676a825542f71a707e9b5a68d8febfc
Kilian David <usr3744@salesianos-lacuesta.net>
2026-03-27 08:17:13 +0000

    Añadimos getters y setters

--- src/net/salesianos/personaje/Personaje.java
+++ src/net/salesianos/personaje/Personaje.java
@@ -11,5 +11,31 @@ public Personaje(String nombre, int nivel) {
     this.nivel = nivel;
     this.energia = 100;
 }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setNivel(int nivel) {
+        if (nivel > 0) {
+            this.nivel = nivel;
+        }
+    }
+
+    public void setEnergia(int energia) {
+        if (energia >= 0 && energia <= 100) {
+            this.energia = energia;
+        }
+    }
+
+  
     
 }
